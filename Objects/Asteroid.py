201426779d510b7434e73df85bfda1674314e46b
Brendanwards <27wardb@mbbc.qld.edu.au>
2024-07-26 13:58:30 +1000

    Added lasers

--- Objects/Asteroid.py
+++ Objects/Asteroid.py
@@ -21,7 +21,8 @@ class Asteroid(RoomObject):
         angle = random.randint(135,225)
         self.set_direction(angle, 10)
 
-
+        # register events
+        self.register_collision_object("Ship")
     def step(self):
         """
         Determines what happens to the asteroid on each tick of the game clock
@@ -47,3 +48,11 @@ class Asteroid(RoomObject):
         if self.x + self.width < 0:
             print("asteroid deleted")
             self.room.delete_object(self)
+ 
+    def handle_collision(self, other, other_type):
+        """
+        Handles the collision events for the Asteroid
+        """
+        
+        if other_type == "Ship":
+            self.room.running = False
